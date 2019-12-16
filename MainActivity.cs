d2e7da28605ba6681be4e13871d9f7312f158bad
unknown <petrjanousek.net@gmail.com>
2019-12-16 14:37:20 +0100

    first source commit

---
+++ MainActivity.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace ButtonAndKeyChecker
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private DateTime _lastKeyPressed = DateTime.MinValue;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);            
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                do
+                {
+                    Thread.Sleep(1000);
+
+                    if (_lastKeyPressed == DateTime.MinValue ||
+                    ((DateTime.Now - _lastKeyPressed).TotalSeconds > 5))
+                    {
+                        RunOnUiThread(() =>
+                        {
+                            SetText("Press a key or button", 20);
+                        });
+                    }
+
+                } while (true);
+            }).Start();
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public void SetText(string text, float size=40)
+        {
+            var ketTexttView = FindViewById<TextView>(Resource.Id.keyTextView);
+            ketTexttView.Text = text;
+            ketTexttView.SetTextSize(Android.Util.ComplexUnitType.Dip, size);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
+        {
+            SetText(keyCode.ToString());
+            _lastKeyPressed = DateTime.Now;
+
+            //return base.OnKeyDown(keyCode, e);
+            return true;
+        }
+    }
+}
+
